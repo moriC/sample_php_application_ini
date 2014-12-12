904372658b5f9eea334225a08f91268852ae5a66
morishita <tomohiro.m0219@gmail.com>
2014-12-13 00:39:06 +0900

    設定ファイルの内容をhogehogeとmogumoguを交互にするように実装してみる

--- index.php
+++ index.php
@@ -6,3 +6,24 @@
 
 	# 連想配列の形式で読み込まれたiniファイルの中身を出力する
 	print_r($ini);
+	echo "<br>";
+
+	# iniファイルの設定を検証する
+	# config.iniファイル内のtest_messageの値を設定する
+	if($ini['test_message'] == "mogumogu"){
+		$ini['test_message'] = "hogehoge";
+	}else{
+		$ini['test_message'] = "mogumogu";
+	}
+
+	# config.iniファイルを開く
+	$fp = fopen('config.ini', 'w');
+
+	# foreachで該当する値を更新する
+	foreach ($ini as $k => $i) fputs($fp, "$k=$i\n");
+
+	# config.iniファイルを閉じる
+	fclose($fp);
+
+	# 連想配列の形式で読み込まれたiniファイルの中身を出力する
+	print_r($ini);
