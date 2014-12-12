c695578653b91647faf1bcaef9058113329439dc
morishita <tomohiro.m0219@gmail.com>
2014-12-13 00:31:15 +0900

    iniファイルを読み込んでみる

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-	echo "PHPアプリケーションでのiniファイルの操作検証";
+	echo "PHPアプリケーションでのiniファイルの操作検証<br>";
+
+	# 同階層にあるconfig.iniファイルを連想配列の形式で読み込む
+	$ini = parse_ini_file('config.ini');
+
+	# 連想配列の形式で読み込まれたiniファイルの中身を出力する
+	print_r($ini);
